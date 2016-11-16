87ae9977cd6fd50772505405a488a7ceea796317
Alexandru Cehan <alex983k@edu.eal.dk>
2016-11-16 13:01:46 +0100

    Showlist complete.

--- GettingRealApp/Program.cs
+++ GettingRealApp/Program.cs
@@ -8,7 +8,11 @@ namespace GettingRealApp
 {
     class Program
     {
-        int[,] array = new int[30, 7];
+
+
+        public static void showList()
+        {
+            string[,] array = new string[6, 5]
         {
         {"Id", "Firstname", "Lastname", "Position", "Adress"},
         {"1", "David", "Bertus", "Attacker", "Somewhere near school"},
@@ -16,17 +20,22 @@ namespace GettingRealApp
         {"3", "Alex", "Damsted", "Midfielder", "Kiwi street, New Zeeland"},
         {"4", "Roxana", "Ion", "Central", "Somewhere else"},
         {"5", "Nikolaj", "Morup", "Center-Back", "Somewhere but not at school :( "}
+        };
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(array[i, 0]+ " " + array[i, 1] + " " + array[i, 2] + " " + array[i, 3] + " " + array[i, 4]);
+            }
         }
-
-
-
-
-
-
 
         static void Main(string[] args)
         {
-
+            string comand;
+            comand = Console.ReadLine();
+            if (comand == "list")
+            {
+                showList();
+            }
+            Console.ReadKey();
         }
     }
 }
