f8193e162ea157020c18a3af851b6e3417575829
Alexandru Cehan <alex983k@edu.eal.dk>
2016-11-16 12:39:37 +0100

    Player list (at the beginning)

---
+++ GettingRealApp/Players.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettingRealApp
+{
+    class Players
+    {
+
+        public int Id { get; set; }
+        public string Lastname { get; set; }
+        public string Firstname { get; set; }
+        public string Position { get; set; }
+        public string Adress { get; set; }
+        public Players(int id, string firstname, string lastname, string position, string adress)
+        {
+
+            Adress = adress;
+            Id = id;
+            Position = position;
+            Firstname = firstname;
+            Lastname = lastname;
+        }
+    }
+}
--- GettingRealApp/Program.cs
+++ GettingRealApp/Program.cs
@@ -8,6 +8,15 @@ namespace GettingRealApp
 {
     class Program
     {
+        int[,] array = new int[30, 7];
+        {
+        {"Id", "Firstname", "Lastname", "Position", "Adress"},
+        {"1", "David", "Bertus", "Attacker", "Somewhere near school"},
+        {"2", "Alex", "Cehan", "Goalkeeper", "Not that close to school"},
+        {"3", "Alex", "Damsted", "Midfielder", "Kiwi street, New Zeeland"},
+        {"4", "Roxana", "Ion", "Central", "Somewhere else"},
+        {"5", "Nikolaj", "Morup", "Center-Back", "Somewhere but not at school :( "}
+        }
 
 
 
